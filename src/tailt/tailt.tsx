28a326789fe9f7f95ebb9c7eb1bafa151310e08b
BerzanXYZ <berzan@tutanota.com>
2022-06-10 11:03:41 +0300

    Added forwarding ref support

--- src/tailt/tailt.tsx
+++ src/tailt/tailt.tsx
@@ -1,118 +1,164 @@
-import { AnchorHTMLAttributes, ButtonHTMLAttributes, FC, FormHTMLAttributes, HTMLAttributes, InputHTMLAttributes, LabelHTMLAttributes, LiHTMLAttributes, SelectHTMLAttributes, SVGAttributes, TextareaHTMLAttributes } from "react";
+import { AnchorHTMLAttributes, ButtonHTMLAttributes, FC, FormHTMLAttributes, forwardRef, HTMLAttributes, InputHTMLAttributes, LabelHTMLAttributes, LiHTMLAttributes, SelectHTMLAttributes, SVGAttributes, TextareaHTMLAttributes } from "react";
 import { clsify } from "./utils";
 
 // These are the functions to create customized components using supported HTML elements
 export function a(t: TemplateStringsArray) {
-    const A: FC<AnchorHTMLAttributes<HTMLAnchorElement>> = ({className = "", ...p}) => <a className={clsify(t) + ' ' + className} {...p}/>
+    const A = forwardRef<HTMLAnchorElement, AnchorHTMLAttributes<HTMLAnchorElement>>(
+        ({className = "", ...p}, ref) => <a ref={ref} className={clsify(t) + " " + className} {...p}/>
+    )
     return A
 }
 
 export function article(t: TemplateStringsArray) {
-    const Article: FC<HTMLAttributes<HTMLElement>> = ({className = "", ...p}) => <article className={clsify(t) + ' ' + className} {...p}/>
+    const Article = forwardRef<HTMLElement, HTMLAttributes<HTMLElement>>(
+        ({className = "", ...p}, ref) => <article ref={ref} className={clsify(t) + " " + className} {...p}/>
+    )
     return Article
 }
 
 export function button(t: TemplateStringsArray) {
-    const Button: FC<ButtonHTMLAttributes<HTMLButtonElement>> = ({className = "", ...p}) => <button className={clsify(t) + ' ' + className} {...p}/>
+    const Button = forwardRef<HTMLButtonElement, ButtonHTMLAttributes<HTMLButtonElement>>(
+        ({className = "", ...p}, ref) => <button ref={ref} className={clsify(t) + " " + className} {...p}/>
+    )
     return Button
 }
 
 export function div(t: TemplateStringsArray) {
-    const Div: FC<HTMLAttributes<HTMLDivElement>> = ({className = "", ...p}) => <div className={clsify(t) + ' ' + className} {...p}/>
+    const Div = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
+        ({className = "", ...p}, ref) => <div ref={ref} className={clsify(t) + " " + className} {...p}/>
+    )
     return Div
 }
 
 export function form(t: TemplateStringsArray) {
-    const Form: FC<FormHTMLAttributes<HTMLFormElement>> = ({className = "", ...p}) => <form className={clsify(t) + ' ' + className} {...p}/>
+    const Form = forwardRef<HTMLFormElement, FormHTMLAttributes<HTMLFormElement>>(
+        ({className = "", ...p}, ref) => <form ref={ref} className={clsify(t) + " " + className} {...p}/>
+    )
     return Form
 }
 
 export function footer(t: TemplateStringsArray) {
-    const Footer: FC<HTMLAttributes<HTMLElement>> = ({className = "", ...p}) => <footer className={clsify(t) + ' ' + className} {...p}/>
+    const Footer = forwardRef<HTMLElement, HTMLAttributes<HTMLElement>>(
+        ({className = "", ...p}, ref) => <footer ref={ref} className={clsify(t) + " " + className} {...p}/>
+    )
     return Footer
 }
 
 export function h1(t: TemplateStringsArray) {
-    const H1: FC<HTMLAttributes<HTMLHeadingElement>> = ({className = "", ...p}) => <h1 className={clsify(t) + ' ' + className} {...p}/>
+    const H1 = forwardRef<HTMLHeadingElement, HTMLAttributes<HTMLHeadingElement>>(
+        ({className = "", ...p}, ref) => <h1 ref={ref} className={clsify(t) + " " + className} {...p}/>
+    )
     return H1
 }
 
 export function h2(t: TemplateStringsArray) {
-    const H2: FC<HTMLAttributes<HTMLHeadingElement>> = ({className = "", ...p}) => <h2 className={clsify(t) + ' ' + className} {...p}/>
+    const H2 = forwardRef<HTMLHeadingElement, HTMLAttributes<HTMLHeadingElement>>(
+        ({className = "", ...p}, ref) => <h2 ref={ref} className={clsify(t) + " " + className} {...p}/>
+    )
     return H2
 }
 
 export function h3(t: TemplateStringsArray) {
-    const H3: FC<HTMLAttributes<HTMLHeadingElement>> = ({className = "", ...p}) => <h3 className={clsify(t) + ' ' + className} {...p}/>
+    const H3 = forwardRef<HTMLHeadingElement, HTMLAttributes<HTMLHeadingElement>>(
+        ({className = "", ...p}, ref) => <h3 ref={ref} className={clsify(t) + " " + className} {...p}/>
+    )
     return H3
 }
 
 export function h4(t: TemplateStringsArray) {
-    const H4: FC<HTMLAttributes<HTMLHeadingElement>> = ({className = "", ...p}) => <h4 className={clsify(t) + ' ' + className} {...p}/>
+    const H4 = forwardRef<HTMLHeadingElement, HTMLAttributes<HTMLHeadingElement>>(
+        ({className = "", ...p}, ref) => <h4 ref={ref} className={clsify(t) + " " + className} {...p}/>
+    )
     return H4
 }
 
 export function h5(t: TemplateStringsArray) {
-    const H5: FC<HTMLAttributes<HTMLHeadingElement>> = ({className = "", ...p}) => <h5 className={clsify(t) + ' ' + className} {...p}/>
+    const H5 = forwardRef<HTMLHeadingElement, HTMLAttributes<HTMLHeadingElement>>(
+        ({className = "", ...p}, ref) => <h5 ref={ref} className={clsify(t) + " " + className} {...p}/>
+    )
     return H5
 }
 
 export function h6(t: TemplateStringsArray) {
-    const H6: FC<HTMLAttributes<HTMLHeadingElement>> = ({className = "", ...p}) => <h6 className={clsify(t) + ' ' + className} {...p}/>
+    const H6 = forwardRef<HTMLHeadingElement, HTMLAttributes<HTMLHeadingElement>>(
+        ({className = "", ...p}, ref) => <h6 ref={ref} className={clsify(t) + " " + className} {...p}/>
+    )
     return H6
 }
 
 export function header(t: TemplateStringsArray) {
-    const Header: FC<HTMLAttributes<HTMLElement>> = ({className = "", ...p}) => <header className={clsify(t) + ' ' + className} {...p}/>
+    const Header = forwardRef<HTMLElement, HTMLAttributes<HTMLElement>>(
+        ({className = "", ...p}, ref) => <header ref={ref} className={clsify(t) + " " + className} {...p}/>
+    )
     return Header
 }
 
 export function input(t: TemplateStringsArray) {
-    const Input: FC<InputHTMLAttributes<HTMLInputElement>> = ({className = "", ...p}) => <input className={clsify(t) + ' ' + className} {...p}/>
+    const Input = forwardRef<HTMLInputElement, InputHTMLAttributes<HTMLInputElement>>(
+        ({className = "", ...p}, ref) => <input ref={ref} className={clsify(t) + " " + className} {...p}/>
+    )
     return Input
 }
 
 export function label(t: TemplateStringsArray) {
-    const Label: FC<LabelHTMLAttributes<HTMLLabelElement>> = ({className = "", ...p}) => <label className={clsify(t) + ' ' + className} {...p}/>
-    return Label
+    const Label = forwardRef<HTMLLabelElement, LabelHTMLAttributes<HTMLLabelElement>>(
+        ({className = "", ...p}, ref) => <label ref={ref} className={clsify(t) + " " + className} {...p}/>
+    )
+   return Label
 }
 
 export function li(t: TemplateStringsArray) {
-    const Li: FC<LiHTMLAttributes<HTMLLIElement>> = ({className = "", ...p}) => <li className={clsify(t) + ' ' + className} {...p}/>
+    const Li = forwardRef<HTMLLIElement, LiHTMLAttributes<HTMLLIElement>>(
+        ({className = "", ...p}, ref) => <li ref={ref} className={clsify(t) + " " + className} {...p}/>
+    )
     return Li
 }
 
 export function main(t: TemplateStringsArray) {
-    const Main: FC<HTMLAttributes<HTMLElement>> = ({className = "", ...p}) => <main className={clsify(t) + ' ' + className} {...p}/>
+    const Main = forwardRef<HTMLElement, HTMLAttributes<HTMLElement>>(
+        ({className = "", ...p}, ref) => <main ref={ref} className={clsify(t) + " " + className} {...p}/>
+    )
     return Main
 }
 
 export function nav(t: TemplateStringsArray) {
-    const Nav: FC<HTMLAttributes<HTMLElement>> = ({className = "", ...p}) => <nav className={clsify(t) + ' ' + className} {...p}/>
-    return Nav
+    const Nav = forwardRef<HTMLElement, HTMLAttributes<HTMLElement>>(
+        ({className = "", ...p}, ref) => <nav ref={ref} className={clsify(t) + " " + className} {...p}/>
+    )
+   return Nav
 }
 
 export function p(t: TemplateStringsArray) {
-    const P: FC<HTMLAttributes<HTMLParagraphElement>> = ({className = "", ...p}) => <p className={clsify(t) + ' ' + className} {...p}/>
+    const P = forwardRef<HTMLParagraphElement, HTMLAttributes<HTMLParagraphElement>>(
+        ({className = "", ...p}, ref) => <p ref={ref} className={clsify(t) + " " + className} {...p}/>
+    )
     return P
 }
 
 export function section(t: TemplateStringsArray) {
-    const Section: FC<HTMLAttributes<HTMLElement>> = ({className = "", ...p}) => <section className={clsify(t) + ' ' + className} {...p}/>
-    return Section
+    const Section = forwardRef<HTMLElement, HTMLAttributes<HTMLElement>>(
+        ({className = "", ...p}, ref) => <section ref={ref} className={clsify(t) + " " + className} {...p}/>
+    )
+   return Section
 }
 
 export function span(t: TemplateStringsArray) {
-    const Span: FC< HTMLAttributes<HTMLSpanElement>> = ({className = "", ...p}) => <span className={clsify(t) + ' ' + className} {...p}/>
-    return Span
+    const Span = forwardRef<HTMLSpanElement, HTMLAttributes<HTMLSpanElement>>(
+        ({className = "", ...p}, ref) => <span ref={ref} className={clsify(t) + " " + className} {...p}/>
+    )
+   return Span
 }
 
 export function textarea(t: TemplateStringsArray) {
-    const TextArea: FC<TextareaHTMLAttributes<HTMLTextAreaElement>> = ({className= "", ...p}) => <textarea className={clsify(t) + ' ' + className} {...p}/>
+    const TextArea = forwardRef<HTMLTextAreaElement, TextareaHTMLAttributes<HTMLTextAreaElement>>(
+        ({className = "", ...p}, ref) => <textarea ref={ref} className={clsify(t) + " " + className} {...p}/>
+    )
     return TextArea
 }
 
 export function ul(t: TemplateStringsArray) {
-    const Ul: FC<HTMLAttributes<HTMLUListElement>> = ({className= "", ...p}) => <ul className={clsify(t) + ' ' + className} {...p}/>
+    const Ul = forwardRef<HTMLUListElement, HTMLAttributes<HTMLUListElement>>(
+        ({className = "", ...p}, ref) => <ul ref={ref} className={clsify(t) + " " + className} {...p}/>
+    )
     return Ul
 }
