94d1ac4c7246263ca0c31076688068dc1a37809e
BerzanXYZ <berzan@tutanota.com>
2022-06-18 14:19:35 +0300

    Beautify html class output

--- src/tailt/utils.ts
+++ src/tailt/utils.ts
@@ -1,5 +1,5 @@
 // clsify() function is to convert Template String Arrays into string.
 // Beautifying strings might be added in the future.
 export function clsify(t: TemplateStringsArray): string {
-    return t.toString()
+    return t.toString().replace(/\n/g, " ")
 }
